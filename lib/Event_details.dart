e86bed2e6ed8631322523dfdc2ced2f3b3fb0981
Keshav Kumar <keshavkumar8738@gmail.com>
2022-11-11 18:03:41 +0530

    1st commit

--- lib/Event_details.dart
+++ lib/Event_details.dart
@@ -5,44 +5,47 @@ import 'package:msocial_scanner_app/models/PostsModels.dart';
 import 'package:http/http.dart' as http;
 
 class EventDetails extends StatefulWidget {
-  const EventDetails({Key? key}) : super(key: key);
+  AttedDetails details;
+  String? id;
+  EventDetails(this.details,this.id);
 
   @override
-  State<EventDetails> createState() => _EventDetailsState();
+  State<EventDetails> createState() => _EventDetailsState(details);
 }
 
 class _EventDetailsState extends State<EventDetails> {
+  AttedDetails? details;
 
-  Future<Post?>? post;
-  List<AttedDetails>? Details = [];
-  List<Data>? postList = [];
+  _EventDetailsState(this.details);
+  // Future<Post?>? post;
+  // List<Data>? postList = [];
 
-  Future<Post> getPostApi() async {
-    final response = await http.get(Uri.parse(
-        'https://api.msocialin.com/api/Authontication_Controller/event_list'));
-    var jsonData = jsonDecode(response.body);
-    if (response.statusCode == 200) {
-      return Post.fromJson(jsonData);
-      // for (Map i in data) {
-      //   postList.add(PostsModels.fromJson(i));
-      // }
-      // return postList;
-    } else {
-      throw Exception('Failed to load');
-    }
-  }
-
-  @override
-  void initState() {
-    post = getPostApi();
-    post!.then((value) {
-      setState(() {
-        Details = value!.data.cast<AttedDetails>();
-        // postList = value!.data;
-      });
-    });
-    super.initState();
-  }
+  // Future<Post> getPostApi() async {
+  //   final response = await http.get(Uri.parse(
+  //       'https://api.msocialin.com/api/Authontication_Controller/event_list'));
+  //   var jsonData = jsonDecode(response.body);
+  //   if (response.statusCode == 200) {
+  //     return Post.fromJson(jsonData);
+  //     // for (Map i in data) {
+  //     //   postList.add(PostsModels.fromJson(i));
+  //     // }
+  //     // return postList;
+  //   } else {
+  //     throw Exception('Failed to load');
+  //   }
+  // }
+  //
+  // @override
+  // void initState() {
+  //   post = getPostApi();
+  //   post!.then((value) {
+  //     setState(() {
+  //       Details = value!.data.cast<AttedDetails>();
+  //       // postList = value!.data;
+  //     });
+  //   });
+  //   super.initState();
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,14 +53,19 @@ class _EventDetailsState extends State<EventDetails> {
         title: Text("Event Details"),
         backgroundColor: Color.fromRGBO(253, 113, 112, 1),
       ),
-      body: ListView.builder(
-          itemCount: Details?.length,
-          itemBuilder: (context, index){
-            return ListTile(
-              title:
-              Text(Details![index].location),
-            );
-          }),
+      body: Center(
+        child: Container(
+          child: Text(details!.startTime),
+        ),
+      )
+      // ListView.builder(
+      //     itemCount: ,
+      //     itemBuilder: (context, index){
+      //       return ListTile(
+      //         title:
+      //         Text(Details![index].location),
+      //       );
+      //     }),
     );
   }
 }
